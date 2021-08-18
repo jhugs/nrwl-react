4dc317812d44649abb62cf122eec191cf18ac0ce
Josh Hughes <jhughes@andculture.com>
2021-08-17 22:07:48 -0400

    initial commit

---
+++ src/app/app.tsx
@@ -0,0 +1,18 @@
+import "./app.css";
+import { Route, Switch } from "react-router";
+import { BrowserRouter as Router } from "react-router-dom";
+import TicketPage from "../pages/tickets-page";
+import TicketDetailPage from "../pages/ticket-detail-page";
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/tickets" component={TicketPage} />
+        <Route exact path="/tickets/:id" component={TicketDetailPage} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;
